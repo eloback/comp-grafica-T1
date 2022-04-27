19b4de44cbd3698053dd91a89bca28d70320aa0d
eduar <eduardolloback@gmail.com>
2022-04-27 09:30:28 -0300

    feat: light1 added, normals and texture added.

--- trab01/trab01.cpp
+++ trab01/trab01.cpp
@@ -1,3 +1,12 @@
+/*
+author: Eduardo Loback, Enzo Redivo
+
+TODO:
+    * camera movement.
+	* texture load.
+    * other lights
+*/
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -22,6 +31,13 @@ struct ivec3 {
 std::vector <vec3> vertices;
 std::vector <vec2> textCoords;
 std::vector <vec3> normals;
+float scale = 0.4;
+vec3 mov{ 0, -20, -105 };
+vec3 cam{ 0, 0, 0 };
+vec3 vr{ 0, 1, 0 };
+enum mode { perpective=0, movement=1, rotation=2 };
+mode actual = movement;
+bool rotating = true;
 
 std::vector < std::vector <ivec3> > faces;
 unsigned int _model;
@@ -35,25 +51,129 @@ vec3 sum(vec3 a, vec3 b) {
     return vec3{ a.x + b.x, a.y + b.y, a.z + b.z };
 }
 
-void calcNormals() {
-    for (int i = 0; i < faces.size(); i++) {
-        vector<ivec3> face = faces[i];
-        vec3 v1 = vertices[face[0].v];
-        vec3 v2 = vertices[face[1].v];
-        vec3 v3 = vertices[face[2].v];
-        vec3 edge = diferrence(v2, v1);
-        vec3 edge2 = diferrence(v3, v1);
-        vec3 normal = vec3{ edge.y * edge2.z - edge.z * edge2.y, edge.z * edge2.x - edge.x * edge2.z, edge.x * edge2.y - edge.y * edge2.x };
-        normal = vec3{ normal.x / sqrt(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z), normal.y / sqrt(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z), normal.z / sqrt(normal.x * normal.x + normal.y * normal.y + normal.z * normal.z) };
-		
-        //cout << "face :" << v1.x << ", " << v1.y << ", " << v1.z <<"; "<< v2.x << ", " << v2.y << ", " << v2.z <<"; "<< v3.x << ", " << v3.y << ", " << v3.z << endl;
-        for (int j = 0; j < face.size(); j++) {
-            ivec3 vertex = face[j];
-          /*  cout << vertex.n << endl;
-            cout<<"normal: "<<normals[vertex.n].x << ", " << normals[vertex.n].y << ", " << normals[vertex.n].z << endl;*/
-        }
-    }
+
+void calcNormals();
+std::vector<string> split(string str, char delimiter);
+void createFace(string line);
+vec2 parseVector2(string line);
+vec3 parseVector3(string line);
+void createList();
+void loadObj(std::string fileName);
+void reshape(int w, int h);
+void draw();
+void display(void);
+void timer(int value);
+void Initialize();
+
+void keyboard(unsigned char key, int x, int y);
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 450);
+    glutInitWindowPosition(20, 20);
+    glutCreateWindow("Carregar OBJ");
+    Initialize();
+    glutReshapeFunc(reshape);
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+    glutTimerFunc(10, timer, 0);
+    loadObj("data/mba1.obj");
+    glutMainLoop();
+    return 0;
 }
+
+void reshape(int w, int h)
+{
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60, (GLfloat)w / (GLfloat)h, 0.1, 3000.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+void draw()
+{	
+    glPushMatrix();
+    glTranslatef(mov.x, mov.y, mov.z);
+    glColor3f(1.0, 0.23, 0.27);
+    glScalef(scale, scale, 0.4);
+    if (!(vr.x == 0 && vr.y == 0 && vr.z == 0)) 
+    {
+        glRotatef(rot, vr.x, vr.y, vr.z);
+        if(rotating) rot = rot + 0.6;
+        if (rot > 360) rot = rot - 360;
+    }
+    //gluLookAt(0, 0, 0, 0 + cam.x, 0 + cam.y, 100 + cam.z, 0, 40, 0
+    glCallList(_model);
+    glPopMatrix();
+    glEnd();
+
+}
+void display(void)
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    draw();
+    glutSwapBuffers();
+}
+
+void timer(int value) {
+    glutPostRedisplay();
+    glutTimerFunc(10, timer, 0);
+}
+
+void Initialize() {
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_NORMALIZE);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+
+    //Ativa o uso de luz
+    glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
+    
+
+    // determina a intensidade e cor da luz
+    //GLfloat lightAmbient[] = { 0.2, 0.2, 0.2, 1.0 }; // cinza
+    //GLfloat lightDiffuse[] = { 0.8, 0.8, 0.8, 1.0 }; // cinza claro
+    GLfloat lightDiffuse[] = { 1.0f, 1.0f, 0.0f, 1.0f };    //yellow diffuse : color where light hit directly the object's surface
+    GLfloat lightAmbient[] = { 1.0f, 0.0f, 0.0f, 1.0f };    //red ambient : color applied everywhere
+    GLfloat luz_especular[] = { 1.0, 1.0, 1.0, 1.0 }; // branco
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, luz_especular);
+
+    GLfloat l1LightAmbient[] = { 0.2, 0.2, 0.2, 1.0 };
+	GLfloat l1LightDiffuse[] = { 0.8, 0.8, 0.8, 1.0 };
+	GLfloat l1LightSpecular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat l1LightPosition[] = { -2.0, 2.0, 1.0, 1.0 };
+	GLfloat l1LightDirection[] = { -1.0, -1.0, 0.0 };
+	
+	glLightfv(GL_LIGHT1, GL_AMBIENT, l1LightAmbient);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, l1LightDiffuse);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, l1LightSpecular);
+	glLightfv(GL_LIGHT1, GL_POSITION, l1LightPosition);
+	glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, l1LightDirection);
+	glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, 45.0);
+	glLightf(GL_LIGHT1, GL_SPOT_EXPONENT, 0.0);
+	
+
+    //determina a posição da luz
+    GLfloat posicao_luz[] = { .5, .5, 0.0, 1.0 };
+    glLightfv(GL_LIGHT0, GL_POSITION, posicao_luz);
+    
+
+    
+}
+
 
 
 std::vector<string> split(string str, char delimiter) {
@@ -73,13 +193,13 @@ std::vector<string> split(string str, char delimiter) {
 
 void createFace(string line) {
     //std::cout << line << endl;
-	std::vector<std::string> tokens = split(line, ' ');
-	const int size = tokens.size();
+    std::vector<std::string> tokens = split(line, ' ');
+    const int size = tokens.size();
     vector<ivec3> face;
     for (int i = 1; i < size; i++) {
         //std::cout << tokens[i] << endl;
         std::vector<std::string> sVertex = split(tokens[i], '/');
-		int sVertexSize = sVertex.size();
+        int sVertexSize = sVertex.size();
         if (sVertexSize == 2) {
             face.push_back(ivec3{ stoi(sVertex[0]) - 1, 0, stoi(sVertex[1]) - 1 });
         }
@@ -92,13 +212,40 @@ void createFace(string line) {
 
 
 vec2 parseVector2(string line) {
-	std::vector<std::string> tokens = split(line, ' ');
-	return vec2{ stof(tokens[1]), stof(tokens[2]) };
+    std::vector<std::string> tokens = split(line, ' ');
+    return vec2{ stof(tokens[1]), stof(tokens[2]) };
 }
 
 vec3 parseVector3(string line) {
-	std::vector<std::string> tokens = split(line, ' ');
-	return vec3{ stof(tokens[1]), stof(tokens[2]), stof(tokens[3]) };
+    std::vector<std::string> tokens = split(line, ' ');
+    return vec3{ stof(tokens[1]), stof(tokens[2]), stof(tokens[3]) };
+}
+
+
+
+void createList() {
+    glNewList(_model, GL_COMPILE);
+    {
+        glPushMatrix();
+        glBegin(GL_TRIANGLES);
+        for (int i = 0; i < faces.size(); i++) {
+            for (int j = 0; j < 3; j++) {
+				vec3 vertex = vertices[faces[i][j].v];
+				vec3 normal = normals[faces[i][j].n];
+				glVertex3f(vertex.x, vertex.y, vertex.z);
+                glNormal3f(normal.x, normal.y, normal.z);
+
+				//texture can not 
+                if (faces[i][j].t != 0) {
+                    vec2 texture = textCoords[faces[i][j].t];
+                    glTexCoord2f(texture.x, texture.y);
+                }
+			}
+        }
+        glEnd();
+        glPopMatrix();
+    }
+    glEndList();
 }
 
 
@@ -136,7 +283,7 @@ void loadObj(std::string fileName) {
                 break;
             case 'f':
                 createFace(line);
-			break;
+                break;
             default: break;
             };
         }
@@ -145,80 +292,60 @@ void loadObj(std::string fileName) {
     {
         std::cerr << "Unable to load mesh: " << fileName << std::endl;
     }
-    cout << "Loaded" << endl;
-	
-    //calcNormals();
 
     cout << "normals calculated" << endl;
-    glNewList(_model, GL_COMPILE);
-    {
-        glPushMatrix();
-        glBegin(GL_TRIANGLES);
-        for (int i = 0; i < faces.size(); i++) {
-            vec3 v1 = vertices[faces[i][0].v];
-			vec3 v2 = vertices[faces[i][1].v];
-			vec3 v3 = vertices[faces[i][2].v];
-			glVertex3f(v1.x, v1.y, v1.z);
-			glVertex3f(v2.x, v2.y, v2.z);
-			glVertex3f(v3.x, v3.y, v3.z);
-        }
-        glEnd();
-		glPopMatrix();
+    createList();
+    cout << "list created" << endl;
+}
+
+
+void keyboard(unsigned char key, int x, int y) {
+    //std::cout << key;
+    switch (key) {
+    case 27:
+        exit(0);
+        break;
+    case 's':
+        if (actual == mode(0)) cam.y = cam.y - 10;
+        else if(actual == mode(1)) mov.y = mov.y - 10;
+        else if (actual == mode(2)) vr.y = vr.y > 1 ? vr.y : vr.y + 0.1;
+        break;
+    case 'w':
+        if (actual == mode(0)) cam.y = cam.y + 10;
+        else if (actual == mode(1)) mov.y = mov.y + 10;
+		else if (actual == mode(2)) vr.y = vr.y < -1 ? vr.y : vr.y - 0.1;
+        break;
+    case 'a':
+        if (actual == mode(0)) cam.x = cam.x - 10;
+        else if (actual == mode(1)) mov.x = mov.x - 10;
+		else if (actual == mode(2)) vr.x = vr.x > 1 ? vr.x : vr.x + 0.1;
+        break;
+    case 'd':
+        if (actual == mode(0)) cam.x = cam.x + 10;
+        else if (actual == mode(1)) mov.x = mov.x + 10;
+		else if (actual == mode(2)) vr.x = vr.x < -1 ? vr.x : vr.x - 0.1;
+        break;
+    case 'f':
+		if (actual == mode(0)) cam.z = cam.z - 10;
+		else if(actual == mode(1)) mov.z = mov.z - 10;
+		else if (actual == mode(2)) vr.z = vr.z > 1 ? vr.z : vr.z + 0.1;
+		break;
+    case 'g':
+		if (actual == mode(0)) cam.z = cam.z + 10;
+		else if (actual == mode(1)) mov.z = mov.z + 10;
+		else if (actual == mode(2)) vr.z = vr.z < -1 ? vr.z : vr.z - 0.1;
+		break;
+    case 'q':
+        if (actual == mode(0)) actual = mode(1);
+        else if (actual == mode(1)) actual = mode(2);
+		else actual = mode(1);
+        cout <<"actual mode is "<< actual << endl;
+        break;
+    case 'r':
+		rotating = !rotating;
+        break;
+    case ' ':
+        scale = scale + 0.2;
+        break;
     }
-    glEndList();
-	cout<< "list created" << endl;
-}
-
-
-void reshape(int w, int h)
-{
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(60, (GLfloat)w / (GLfloat)h, 0.1, 3000.0);
-
-    glMatrixMode(GL_MODELVIEW);
-}
-void draw()
-{
-    glClear(GL_DEPTH_BUFFER_BIT);
-    glPushMatrix();
-    glTranslatef(0, -40.00, -105);
-    glColor3f(1.0, 0.23, 0.27);
-    glScalef(0.4, 0.4, 0.4);
-    glRotatef(rot, 0, 1, 0);
-    glCallList(_model);
-    glPopMatrix();
-    rot = rot + 0.6;
-    if (rot > 360) rot = rot - 360;
-}
-void display(void)
-{
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-    draw();
-    glutSwapBuffers();
-}
-
-void timer(int value) {
-    glutPostRedisplay();
-    glutTimerFunc(10, timer, 0);
-}
-
-int main(int argc, char** argv)
-{
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glutInitWindowSize(800, 450);
-    glutInitWindowPosition(20, 20);
-    glutCreateWindow("Carregar OBJ");
-    glutReshapeFunc(reshape);
-    glutDisplayFunc(display);
-    glutTimerFunc(10, timer, 0);
-    loadObj("data/mba1.obj");
-    glutMainLoop();
-    return 0;
 }
